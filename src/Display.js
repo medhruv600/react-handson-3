810961a5a8a2fd55e5166fba0249cc93c61410e5
medhruv600 <medhruv600@gmail.com>
2023-04-03 17:13:01 +0545

    warning sorted!

--- src/Display.js
+++ src/Display.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export default class Display extends Component {
   constructor(props) {
     super(props);
-    // console.log(props);
+    console.log(props);
   }
   render() {
     return (
